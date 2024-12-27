249d363e780ec47f8f46085da2def1e06d263f83
Kelley van Evert <kelley@ijsfontein.nl>
2024-12-27 12:23:35 +0100

    Solved day 22 bonus

--- day22/rust/src/main.rs
+++ day22/rust/src/main.rs
@@ -1,4 +1,6 @@
 use cached::proc_macro::cached;
+use fxhash::FxHashMap;
+use itertools::Itertools;
 use std::time::Instant;
 
 fn main() {
@@ -9,10 +11,10 @@ fn main() {
         println!("First part: {}", solve(input));
     });
 
-    // time(|| {
-    //     // ..
-    //     println!("Bonus: {}", bonus(input));
-    // });
+    time(|| {
+        // ±1s
+        println!("Bonus: {}", bonus(input));
+    });
 }
 
 // macro_rules! vprintln {
@@ -30,9 +32,9 @@ where
 
 #[cached]
 fn next_secret(secret: usize) -> usize {
-    let secret = ((secret * 64) ^ secret) % 16777216;
-    let secret = ((secret / 32) ^ secret) % 16777216;
-    let secret = ((secret * 2048) ^ secret) % 16777216;
+    let secret = ((secret << 06) ^ secret) % 0b1000000000000000000000000;
+    let secret = ((secret >> 05) ^ secret) % 0b1000000000000000000000000;
+    let secret = ((secret << 11) ^ secret) % 0b1000000000000000000000000;
     secret
 }
 
@@ -50,15 +52,74 @@ fn solve(input: &str) -> usize {
         .lines()
         .map(|line| {
             let secret = line.parse::<usize>().unwrap();
-            let result = apply(next_secret, 2000, secret);
-            // println!("{secret}: {result}");
-            result
+            apply(next_secret, 2000, secret)
         })
         .sum()
 }
 
 fn bonus(input: &str) -> usize {
-    0
+    let initial_secrets = input
+        .trim()
+        .lines()
+        .map(|line| line.parse::<usize>().unwrap())
+        .collect_vec();
+
+    let infos = initial_secrets
+        .iter()
+        .map(|&secret| {
+            // println!("Secret {secret}");
+
+            let n = 2000;
+            let secret = secret; // 123
+
+            let mut seq = vec![0; n + 1];
+            seq[0] = secret;
+            for i in 0..n {
+                seq[i + 1] = next_secret(seq[i]);
+            }
+
+            // println!("  seq: {:?}", seq);
+
+            let diffseq = seq
+                .iter()
+                .tuple_windows()
+                .map(|(a, b)| (b % 10) as i32 - (a % 10) as i32)
+                .collect_vec();
+
+            // println!("  diffs: {:?}", diffseq);
+
+            let mut found = FxHashMap::default();
+
+            for (i, (&a, &b, &c, &d)) in diffseq.iter().tuple_windows().enumerate() {
+                let t = (a, b, c, d);
+                let price = seq[i + 4] % 10;
+                // println!("    t = {t:?} -> {price}");
+
+                if !found.contains_key(&t) {
+                    found.insert(t, price);
+                }
+            }
+
+            // println!("  has? {:?}", found.get(&t));
+
+            (seq, diffseq, found)
+        })
+        .collect_vec();
+
+    let mut totals = FxHashMap::default();
+
+    for (_, _, found) in &infos {
+        for (&t, &price) in found {
+            totals
+                .entry(t)
+                .and_modify(|total| {
+                    *total += price;
+                })
+                .or_insert(price);
+        }
+    }
+
+    totals.values().cloned().max().unwrap()
 }
 
 #[test]
@@ -75,21 +136,15 @@ fn test() {
         37327623
     );
 
-    //     assert_eq!(
-    //         bonus(
-    //             "
-    // r, wr, b, g, bwu, rb, gb, br
-
-    // brwrr
-    // bggr
-    // gbbr
-    // rrbgbr
-    // ubwu
-    // bwurrg
-    // brgr
-    // bbrgwb
-    // "
-    //         ),
-    //         16
-    //     );
+    assert_eq!(
+        bonus(
+            "
+1
+2
+3
+2024
+"
+        ),
+        23
+    );
 }
